60a7d727923f40b6adc322b3842856933dec664a
Martin Chong <martinmanho@gmail.com>
2019-04-05 18:12:12 -0400

    Uploading 2nd day progress of vue_test1

---
+++ vue_test1/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <Todos v-bind:todos="todos"/>
+  </div>
+</template>
+ 
+<script>
+import Todos from './components/ToDos'; 
+
+export default {
+  name: 'app',
+  components: {
+    Todos
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "ToDo 1",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "ToDo 2",
+          completed: true
+        },
+        {
+          id: 3,
+          title: "ToDo 3",
+          completed: false
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+  *{
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
+</style>
---
+++ vue_test1/src/components/ToDos.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div v-bind:key="todo.id" v-for="todo in todos">
+            <ToDoItem v-bind:todo="todo"/>
+        </div>
+    </div>
+</template>
+
+<script>
+import ToDoItem from './ToDoItem.vue';
+
+export default { 
+    name: "ToDos",
+    components: {
+        ToDoItem
+    },
+    props: ["todos"]
+}
+</script>
+
+<style <style scoped>
+
+</style>
